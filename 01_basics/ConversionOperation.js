ab12418f864ba4fbe4216a25ef0cb4a85d3fc8b4
shambhu pandey <156665861+shambhu-pandey@users.noreply.github.com>
2024-01-15 14:28:54 +0000

    conversion

--- 01_basics/ConversionOperation.js
+++ 01_basics/ConversionOperation.js
@@ -1,116 +1,119 @@
-let score = " hitesh";
+// let score = " hitesh";
 
-console.log(score);
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(score);
+// console.log(typeof score);
+// console.log(typeof (score));
 
-// string to number conversion
-let valueInNumber= Number(score);
-console.log(valueInNumber);
-console.log(typeof valueInNumber);
-
-
-//******************************** */
-let num ="33";
-console.log(num);
-console.log(typeof num);
-
-// string to number.
-let valueinNumber = Number(num);
-console.log(typeof valueinNumber);
-console.log(num);
+// // string to number conversion
+// let valueInNumber= Number(score);
+// console.log(valueInNumber);
+// console.log(typeof valueInNumber);
 
 
-//***************************** */
-let numb ="33abc";
-console.log(numb);
-console.log(typeof numb);
+// //******************************** */
+// let num ="33";
+// console.log(num);
+// console.log(typeof num);
 
-// string to number
-let valueINnumber = Number(numb);
-console.log(valueINnumber);
-console.log(typeof valueINnumber);
-
-//***************************** */
-let bool =true;
-console.log(bool);
-console.log(typeof bool);
-
-// boolean to number
-let inNumber= Number(bool);
-console.log(inNumber);
-console.log(typeof inNumber);  
-// true => 1; false => 0;
-
-//*********************************** */
-// string to boolean 
-let isLoggedIn = "hitesh";
-let booleanInLogged =Boolean(isLoggedIn);
-console.log(booleanInLogged);
-console.log(typeof booleanInLogged );
-
-//**************************** */
-let number =1;
-let inBoolean =Boolean(number);
-console.log(inBoolean);
-console.log(typeof inBoolean);
-
-// 1 =>  true ; 0 => false
-
-// ************************/
-let str ="";
-console.log(str);
-let inboolean =Boolean(str);
-console.log(inboolean);
-
-// "" => false
-//"hitesh" => true 
-
-// ******************** ************
-// number to string 
-let someNumber = 33;
-console.log(someNumber);
-console.log(typeof someNumber);
-
-let inString = String(someNumber);
-console.log(inString);
-console.log(typeof inString);
-
-//*****************Operation****************** */
-let value= 3;
-let negvalue= -value;
-console.log(negvalue);
-
-console.log(4+5);
-console.log(4-5);
-console.log(2**3);
+// // string to number.
+// let valueinNumber = Number(num);
+// console.log(typeof valueinNumber);
+// console.log(num);
 
 
-let str1 ="hello";
-let str2 =" hitesh";
+// //***************************** */
+// let numb ="33abc";
+// console.log(numb);
+// console.log(typeof numb);
 
-let str3=str1+ str2;
-console.log(str3);
+// // string to number
+// let valueINnumber = Number(numb);
+// console.log(valueINnumber);
+// console.log(typeof valueINnumber);
 
-console.log("1" + 4);
-console.log(5+"3");
-console.log(1+3+4);
-console.log("1" +3+"4");
-console.log("1" +2 +1) 
-console.log(1 + 2 + "2");
-console.log( (3 + 4) * 5 % 3);
-console.log(true);
-console.log(+true);
- console.log(+"");
- console.log("+");
- console.log("");
- console.log(" ");
+// //***************************** */
+// let bool =true;
+// console.log(bool);
+// console.log(typeof bool);
 
- let gameCounter=100;
- gameCounter++;
- console.log(gameCounter);
- ++gameCounter;
- console.log(gameCounter);
+// // boolean to number
+// let inNumber= Number(bool);
+// console.log(inNumber);
+// console.log(typeof inNumber);  
+// // true => 1; false => 0;
+
+// //*********************************** */
+// // string to boolean 
+// let isLoggedIn = "hitesh";
+// let booleanInLogged =Boolean(isLoggedIn);
+// console.log(booleanInLogged);
+// console.log(typeof booleanInLogged );
+
+// //**************************** */
+// let number =1;
+// let inBoolean =Boolean(number);
+// console.log(inBoolean);
+// console.log(typeof inBoolean);
+
+// // 1 =>  true ; 0 => false
+
+// // ************************/
+// let str ="";
+// console.log(str);
+// let inboolean =Boolean(str);
+// console.log(inboolean);
+
+// // "" => false
+// //"hitesh" => true 
+
+// // ******************** ************
+// // number to string 
+// let someNumber = 33;
+// console.log(someNumber);
+// console.log(typeof someNumber);
+
+// let inString = String(someNumber);
+// console.log(inString);
+// console.log(typeof inString);
+
+
+// //*****************Operation****************** */
+
+
+// let value= 3;
+// let negvalue= -value;
+// console.log(negvalue);
+
+// console.log(4+5);
+// console.log(4-5);
+// console.log(2**3);
+
+
+// let str1 ="hello";
+// let str2 =" hitesh";
+
+// let str3=str1+ str2;
+// console.log(str3);
+
+// console.log("1" + 4);
+// console.log(5+"3");
+// console.log(1+3+4);
+// console.log("1" +3+"4");
+// console.log("1" +2 +1) 
+// console.log(1 + 2 + "2");
+// console.log( (3 + 4) * 5 % 3);
+// console.log(true);
+// console.log(+true);
+//  console.log(+"");
+//  console.log("+");
+//  console.log("");
+//  console.log(" ");
+
+//  let gameCounter=100;
+//  gameCounter++;
+//  console.log(gameCounter);
+//  ++gameCounter;
+//  console.log(gameCounter);
 
 
 
@@ -145,3 +148,7 @@ console.log(+true);
 
 
 
+let gamecounter = 100;
+console.log(++gamecounter);
+console.log(gamecounter++);
+console.log(gamecounter++);

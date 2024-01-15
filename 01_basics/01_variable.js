e6a4bd598ec33b32915b5a49b9206720d9b8ee38
shambhu pandey <156665861+shambhu-pandey@users.noreply.github.com>
2024-01-15 14:03:46 +0000

    basic variable and datattypes

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,15 @@
+const accountId = 12345;
+let name ="shambhu";
+var accountPassword = "1233456";
+accountCity ="patna";
+
+// accountId=2222;  // it will not allow to change the accountID because const
+
+// let name = "shambhu pandey"; it will not allowed 
+ name = 123456789; // allowed 
+ name ="shambhu pandey "// allowed 
+var accountPassword= "shambhu@123";
+
+
+console.log([accountId , name , accountPassword , accountCity]);
+console.table([accountId , name , accountPassword , accountCity]);
---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,18 @@
+console.log(6+6);
+
+console.log("shambhu pandey");
+let name = "shambhu";
+let age = 18 ;
+let isLogged = false;
+
+
+// number  = 2 to power 53 
+// bigint 
+// string => ""
+// null => standalone value 
+// undefined => yet not defined
+// symbol => unique 
+
+
+console.log(typeof null);
+console.log(typeof undefined);
